d2831a3126d4adb18206d4dea244c61d8cecb16c
Haibo <ghbjy1128@gmail.com>
2025-02-06 22:35:32 +0800

    feat: enhance GlobalSidebar component with logo and dynamic menu items; add SVG logo and update menu structure

--- app/src/components/GlobalSidebar.vue
+++ app/src/components/GlobalSidebar.vue
@@ -1,16 +1,60 @@
 <template>
   <el-aside class="global-sidebar" ref="sidebar" :class="{ 'collapsed': isCollapsed }">
     <header>
+      <h1 class="logo">
+        <svg viewBox="0 0 688 166.4">
+          <polygon points="62.5 0 0 29.7 0 48.3 45.2 27.7 45.2 166.4 62.4 166.4 62.5 0" class="one"></polygon>
+          <polyline points="0 77.7 19 69 19 166.4 36.2 166.4 36.2 41.3 0 58.6" class="one">
+          </polyline>
+          <g class="tu">
+              <polygon points="209.6 0 88.8 0 82.2 0 82.2 8.4 82.2 8.8 82.2 17.2 88.8 17.2 209.6 17.2 215 17.2 215 8.8 215 8.4 215 0 209.6 0">
+              </polygon>
+              <path d="M143.5,166.4H126.3V45.2h-44V28h61.2Z"></path>
+              <path d="M153.8,166.4H171V45.2h44V28H153.8Z"></path>
+              <path d="M299,138c21.7,0,35.9-17.8,35.9-39.6V0H317.5V98.4c0,12.3-6.4,22.3-18.5,22.3s-18.5-10-18.5-22.3V0H263.1V98.4C263.2,120.2,277.3,138,299,138Z">
+              </path>
+              <path d="M345.4,0V94.9c0,30-18.9,54.5-46.4,54.5s-46.4-24.4-46.4-54.5V0h-17V94.9c0,28.4,13.7,52.9,35.2,64.5a58.2,58.2,0,0,0,28.1,7.1h.3c36.9,0,63.5-32.1,63.5-71.5V0Z">
+              </path>
+          </g>
+          <g class="cn">
+              <polygon points="574.5 110.4 574.5 94.4 636.2 94.4 638.4 90.8 660.6 90.8 647.6 110.4 574.5 110.4">
+              </polygon>
+              <polygon points="574.5 133.9 574.5 117.9 636.2 117.9 638.4 114.3 660.6 114.3 647.6 133.9 574.5 133.9">
+              </polygon>
+              <polygon points="603.9 52.5 618.8 62.8 644.1 45.1 597.6 45.1 594.5 49.1 574.5 49.1 591.3 25 610.2 25 606.6 29.2 660.6 29.2 660.6 52.5 635.3 67.2 660.6 73 660.6 87.2 617.8 77.3 574.5 87.2 574.5 72.6 599.6 67.2 574.5 52.5 603.9 52.5">
+              </polygon>
+              <path d="M547.1.6V158.3H688V.6ZM670.9,141.2H564.2V17.8H670.9Z"></path>
+              <path d="M490.2,143v-9.9h24.5V91.5H408.6v41.6h25.5V143H392.4v15.4H531.8V143Zm-65.5-36.6h73.8v11.2H424.7Zm24.8,26.7h25.3V143H449.5Z">
+              </path>
+              <path d="M531.9,46.4H392.3v35h16.9v5.2H515V81.4h16.9ZM516.5,71H407.8V61.9H516.5Z">
+              </path>
+              <polygon points="531.9 21.2 532 5.7 469.8 5.7 469.8 0.2 454.4 0.2 454.4 5.7 392.3 5.7 392.3 21.2 454.4 21.2 454.4 26 401.8 26 401.8 41.5 522.4 41.5 522.4 26 469.8 26 469.8 21.2 531.9 21.2">
+              </polygon>
+          </g>
+        </svg>
+      </h1>
     </header>
 
     <nav>
       <ul>
-        <li v-for="menu in menus" :key="menu.name">
-          <router-link :to="menu.path" @click="openSubMenu(menu, $event)">
-            <i v-if="menu.icon" :class="'icon-' + menu.icon"></i>
-            <span>{{ menu.name }}</span>
-            <i v-if="menu.sub_menus" class="icon-arrow-down"></i>
-          </router-link>
+        <li v-for="(menu, index) in menus" :key="index" :class="['menu-item', 'menu-type-' + menu.type, { 'active': route.meta === menu.meta }]">
+          <template v-if="menu.type === 'link'">
+            <router-link :to="menu.path" @click="openSubMenu(menu, $event)">
+              <component :is="menu.icon" />
+              <span>{{ menu.name }}</span>
+            </router-link>
+          </template>
+
+          <template v-else-if="menu.type === 'fun'">
+            <span @click="menu.cb">
+              <component :is="menu.icon" />
+              <span>{{ menu.name }}</span>
+            </span>
+          </template>
+
+          <template v-else-if="menu.type === 'hr'">
+            <hr>
+          </template>
         </li>
       </ul>
 
@@ -22,53 +66,73 @@
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue';
+import { ref, defineAsyncComponent, onMounted } from 'vue';
 import { useGlobalStore } from '@/stores/Global';
 import { useRouter } from 'vue-router';
 import emitter from '@/utils/EventBus';
+// import { Service } from '@element-plus/icons-vue';
 
 const router = useRouter();
 const route = router.currentRoute;
 
 const GlobalStore = useGlobalStore();
 const isCollapsed = ref(false);
-
 const menus = ref([
-  {
-    name: 'Overview',
-    icon: 'home',
+{
+    name: '首页',
+    type: 'link',
+    icon: 'HomeFilled',
     path: '/',
-    meta: 'overview'
+    meta: 'home'
   },
   {
-    name: 'Manage',
-    icon: 'palette',
-    path: '/manage',
-    meta: 'manage',
+    name: '探索',
+    type: 'link',
+    icon: 'CoffeeCup',
+    path: '/explore',
+    meta: 'explore'
   },
   {
-    name: 'Analytics',
-    icon: 'analytics',
-    path: '/analytics',
-    meta: 'analytics',
+    name: '个人主页',
+    type: 'link',
+    icon: 'User',
+    path: '/personal',
+    meta: 'personal',
   },
   {
-    name: 'Messages',
-    icon: 'comments',
-    path: '/messages',
-    meta: 'messages',
+    name: '资产',
+    type: 'link',
+    icon: 'Box',
+    path: '/asset',
+    meta: 'asset',
   },
   {
-    name: 'Earnings',
-    icon: 'pig',
-    path: '/earnings',
-    meta: 'earnings'
+    name: '图片生成',
+    type: 'link',
+    icon: 'MagicStick',
+    path: '/image/generate',
+    meta: 'generate-image',
   },
   {
-    name: 'Social Insights',
-    icon: 'line',
-    path: '/social-insights',
-    meta: 'social-insights'
+    type: 'hr',
+  },
+  {
+    name: '消息中心',
+    type: 'fun',
+    icon: 'Bell',
+    meta: 'notification',
+    cb: () => {
+      emitter.emit('open-notification');
+    }
+  },
+  {
+    name: '邀请',
+    type: 'fun',
+    icon: 'Message',
+    meta: 'social-insights',
+    cb: () => {
+      emitter.emit('open-invite');
+    }
   }
 ]);
 
@@ -118,66 +182,29 @@ onMounted(() => {
   & > header {
     padding: 24px 16px 0;
 
-    h1 {
-      display: flex;
 
-      .heading {
-        display: none;
-        font: var(--demi-24);
-        color: var(--Text-primary);
+    .logo {
+      height: 32px;
+      position: relative;
+
+      svg {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
       }
 
-      .logo {
-        flex: 1;
-        color: var(--Text-brand);
-        width: 172px;
-        height: 40px;
-        position: relative;
-
-        svg {
-          position: absolute;
-          transition: var(--sidebar-collapse-transition);
-        }
-
-        .svg-icon__logo-text {
-          width: 100%;
-          height: 100%;
-          opacity: 1;
-          left: -16px;
-        }
-
-        .svg-icon__logo-a {
-          width: 54px;
-          height: 54px;
-          opacity: 0;
-          left: -16px;
-          top: -2px;
-        }
+      .one {
+        fill: var(--Text-brand);
       }
 
-      i {
-        height: 24px;
-        width: 24px;
-        cursor: pointer;
-        border-radius: var(--radius);
-        transition: all 0.2s ease;
-        color: var(--Black-40);
-        position: fixed;
-        z-index: 9;
-        top: 26px;
-        left: 226px;
-        transition: var(--sidebar-collapse-transition);
+      .tu {
+        fill: var(--Text-primary);
+      }
 
-        svg {
-          width: 100%;
-          height: 100%;
-          transition: var(--sidebar-collapse-transition);
-        }
-
-        &:hover {
-          background-color: var(--Neu-30);
-          color: var(--Text-brand);
-        }
+      .cn {
+        fill: var(--Text-default);
       }
     }
 
@@ -187,7 +214,7 @@ onMounted(() => {
   }
 
   & > nav {
-    padding: 16px 0;
+    padding: 8px 0;
 
     & > ul {
       padding: 8px;
@@ -195,7 +222,9 @@ onMounted(() => {
       & > li {
         margin: 8px 0;
 
+        & > span,
         & > a {
+          cursor: pointer;
           display: flex;
           align-items: center;
           padding: 0 12px;
@@ -203,8 +232,9 @@ onMounted(() => {
           transition: var(--sidebar-collapse-transition);
           color: var(--Text-default);
           text-decoration: none;
-          height: 44px;
-          font: var(--medium-16);
+          height: 40px;
+          font-size: 14px;
+          font-weight: 400;
           transition: var(--sidebar-collapse-transition);
 
           &.router-link-active,
@@ -220,9 +250,9 @@ onMounted(() => {
           }
 
           & > svg {
-            width: 20px;
-            height: 20px;
-            margin-right: 8px;
+            width: 16px;
+            height: 16px;
+            margin-right: 10px;
             transition: var(--sidebar-collapse-transition);
           }
 
@@ -258,6 +288,12 @@ onMounted(() => {
             background-color: var(--Neu-30);
             color: var(--Text-primary);
           }
+        }
+
+        hr {
+          border: 0;
+          border-top: 1px solid var(--Black-10);
+          margin: 16px 0;
         }
       }
     }
